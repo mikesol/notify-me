697ba1e00f670213b635408c126d0b48431e1d07
Mike Solomon <mike@meeshkan.com>
2019-05-26 15:34:36 +0300

    Adds notification picture

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,8 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      A project that fails. A lot. See
+      <a href="https://medium.com/@Meeshkan" target="_blank" rel="noopener">this</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
